6014e0da389745d6794b97875a64e4ac67d56467
karry3775 <kartikprakash3775@gmail.com>
2020-12-22 10:05:15 -0500

    vio: switched to pangolin for viz

--- src/visual_odometry/include/visual_odometry/PlottingUtils.h
+++ src/visual_odometry/include/visual_odometry/PlottingUtils.h
@@ -1,10 +1,6 @@
 #pragma once
 
-#include <boost/python.hpp>
-#include <iostream>
-#include <Python.h>
-#include <gtkmm-3.0/gtkmm.h>
-
+#include <pangolin/pangolin.h>
 
 void plot();
 
--- src/visual_odometry/src/PlottingUtils.cpp
+++ src/visual_odometry/src/PlottingUtils.cpp
@@ -1,20 +1,32 @@
- #include <visual_odometry/PlottingUtils.h>
+#include <visual_odometry/PlottingUtils.h>
+ 
+void plot(){
 
+    pangolin::CreateWindowAndBind("Main", 640, 480);
+    glEnable(GL_DEPTH_TEST);
 
- // source : https://stackoverflow.com/questions/765408/free-easy-way-to-draw-graphs-and-charts-in-c
- void plot(){
-    try {
-        // Python startup code
-        Py_Initialize();
-        PyRun_SimpleString("import signal");
-        PyRun_SimpleString("signal.signal(signal.SIGINT, signal.SIG_DFL)");
+    // Define Projection and initial ModelView matrix
+    pangolin::OpenGlRenderState s_cam(
+        pangolin::ProjectionMatrix(640,480,420,420,320,240,0.2,100),
+        pangolin::ModelViewLookAt(-2,2,-2, 0,0,0, pangolin::AxisY)
+    );
 
+    // Create Interactive View in window
+    pangolin::Handler3D handler(s_cam);
+    pangolin::View& d_cam = pangolin::CreateDisplay()
+            .SetBounds(0.0, 1.0, 0.0, 1.0, -640.0f/480.0f)
+            .SetHandler(&handler);
 
-        // Normal Gtk startup code
-        Gtk::Main kit(0,0); // TODO : get the cmakelists fixed
-        
-    } catch (...) {
-        std::cout << "Error occured!" ;
+    while( !pangolin::ShouldQuit() )
+    {
+        // Clear screen and activate view to render into
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        d_cam.Activate(s_cam);
+
+        // Render OpenGL Cube
+        pangolin::glDrawColouredCube();
+
+        // Swap frames and Process Events
+        pangolin::FinishFrame();
     }
-
 }
