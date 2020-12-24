9466d6ce1fd786a5585d2bef28032c8e25521bf0
karry3775 <kartikprakash3775@gmail.com>
2020-12-24 13:28:58 -0500

    vio: code restructing and calibration file parsing
    Added setters and getters and added code that will accept the calib file path and
    set values for calib params

--- src/visual_odometry/src/visual_odometry_node.cpp
+++ src/visual_odometry/src/visual_odometry_node.cpp
@@ -9,13 +9,22 @@ int main (int argc, char **argv) {
 
     vslam::VisualOdometry* voObj = new vslam::VisualOdometry(argc, argv); 
 
-    // vslam::getCommandLineInformation(argc, argv, voObj);
+    voObj->setImageAndCalibPaths(argc, argv, voObj);
+    voObj->setCalibParams();
+
+    // Print out if we can see the P0_
+    LOG(INFO) << "The value for P0_ is: " << voObj->getP0_();
+    LOG(INFO) << "The value for P1_ is: " << voObj->getP1_();
+    LOG(INFO) << "The value for P2_ is: " << voObj->getP2_();
+    LOG(INFO) << "The value for P3_ is: " << voObj->getP3_();
+    LOG(INFO) << "The value for Tr_ is: " << voObj->getTr_();
 
 	// voObj->readImages();
 
+
 	// std::cout << "The object data dir is" << voObj->dataDir << std::endl;
 
-    plot();
+    // vslam::drawFakeMap();
 
     return 0;
 }
